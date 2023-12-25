17c9f24b1e00b049358ac619cef90bd2e61e1ebd
matsuotetsuji <103009758+matsuotetsuji@users.noreply.github.com>
2023-12-25 17:46:50 +0900

    Add ShouldBeStrict

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+
+        /** @var \Illuminate\Foundation\Application $app */
+        // https://stackoverflow.com/questions/75671705/isproduction-method-is-not-detected-in-this-app-isproduction-in-laravel
+        $app = $this->app;
+
+        // 本番環境以外で有効にする
+        // Model::preventAccessingMissingAttributes(! $this->app->isProduction());
+        Model::shouldBeStrict(! $app->isProduction());
     }
 }
